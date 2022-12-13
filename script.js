5e000d2b05c1cdd3044730d75a1109caa20983d1
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-13 21:31:28 +0100

    add accordion section

--- script.js
+++ script.js
@@ -39,3 +39,39 @@ const leftPresents = () => {
     sendBtn.disabled = false;
   }
 };
+
+// ACCORDION
+
+const accordion = document.querySelector(".accordion");
+const accordionBtns = document.querySelectorAll(".accordion-btn");
+
+function openAccordionItems() {
+  if (this.nextElementSibling.classList.contains("active")) {
+    this.nextElementSibling.classList.remove("active");
+  } else {
+    closeAccordionItems();
+    this.nextElementSibling.classList.toggle("active");
+  }
+}
+
+function closeAccordionItems() {
+  const allActiveItems = document.querySelectorAll(".accordion-info");
+  allActiveItems.forEach((item) => item.classList.remove("active"));
+}
+
+const clickOutsideAccordion = (e) => {
+  if (
+    e.target.classList.contains("accordion-btn") ||
+    e.target.classList.contains("accordion-info") ||
+    e.target.classList.contains("accordion-info-text")
+  )
+    return;
+
+  closeAccordionItems();
+};
+
+accordionBtns.forEach((btn) =>
+  btn.addEventListener("click", openAccordionItems)
+);
+
+window.addEventListener("click", clickOutsideAccordion);
