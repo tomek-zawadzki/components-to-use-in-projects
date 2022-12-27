6cd88b9dcb9196278d1a75d82cd522615e3642f5
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-27 21:39:43 +0100

    add fake form section

--- script.js
+++ script.js
@@ -288,7 +288,6 @@ const handleScrollBar = () => {
     document.body.getBoundingClientRect().height - window.innerHeight;
 
   const scrollBarWidth = Math.floor((scroll / leftToScroll) * 100);
-  console.log(scrollBarWidth);
 
   root.style.setProperty("--scroll-width", `${scrollBarWidth}%`);
 
@@ -308,3 +307,77 @@ const scrollToTop = () => {
 
 window.addEventListener("scroll", handleScrollBar);
 scrollToTopBtn.addEventListener("click", scrollToTop);
+
+// FAKE FORM
+
+const formPages = document.querySelectorAll(".page");
+const steps = document.querySelectorAll(".step");
+const progressBar = document.querySelector(".progress-bar");
+const prevBtn = document.querySelector(".btn-form-prev");
+const nextBtn = document.querySelector(".btn-form-next");
+
+let currentStep = 1;
+
+const handleNextBtn = () => {
+  currentStep++;
+
+  if (currentStep > steps.length) {
+    currentStep = steps.length;
+  }
+
+  handleProgressBar();
+};
+
+const handlePrevBtn = () => {
+  currentStep--;
+
+  if (currentStep < 1) {
+    currentStep = 1;
+  }
+
+  handleProgressBar();
+};
+
+const handleProgressBar = () => {
+  steps.forEach((step, index) => {
+    if (index < currentStep) {
+      step.classList.add("active-step");
+    } else {
+      step.classList.remove("active-step");
+    }
+  });
+
+  const activeSteps = document.querySelectorAll(".active-step");
+  progressBar.style.width =
+    ((activeSteps.length - 1) / (steps.length - 1)) * 100 + "%";
+
+  handleButtons();
+  handleFormPages();
+};
+
+const handleButtons = () => {
+  if (currentStep === 1) {
+    prevBtn.disabled = true;
+  } else if (currentStep === steps.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+};
+
+const handleFormPages = () => {
+  formPages.forEach((page) => {
+    console.log(currentStep);
+    console.log(page.dataset.num);
+    console.log(page);
+    if (currentStep == page.dataset.num) {
+      page.classList.add("active-page");
+    } else {
+      page.classList.remove("active-page");
+    }
+  });
+};
+
+nextBtn.addEventListener("click", handleNextBtn);
+prevBtn.addEventListener("click", handlePrevBtn);
