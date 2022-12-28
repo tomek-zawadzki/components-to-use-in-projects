94f127be4733269ab0a12f4a9abeb6928b402cd1
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-28 13:23:20 +0100

    add cookie alert

--- script.js
+++ script.js
@@ -381,3 +381,22 @@ const handleFormPages = () => {
 
 nextBtn.addEventListener("click", handleNextBtn);
 prevBtn.addEventListener("click", handlePrevBtn);
+
+// COOKIE
+const cookieBox = document.querySelector(".cookie-box");
+const cookieBtn = document.querySelector(".cookie-btn");
+
+const showCookie = () => {
+  const cookieEaten = localStorage.getItem("cookie");
+  if (cookieEaten) {
+    cookieBox.classList.add("hide");
+  }
+};
+
+const handleCookieBox = () => {
+  localStorage.setItem("cookie", "true");
+  cookieBox.classList.add("hide");
+};
+
+cookieBtn.addEventListener("click", handleCookieBox);
+showCookie();
